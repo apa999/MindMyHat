0f29f7f1ee79b9d1098d42f00c67888027fec348
apa999 <apa999@me.com>
2023-05-07 16:03:07 +0100

    Initial Commit

---
+++ MindMyHat/MindMyHatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindMyHatApp.swift
+//  MindMyHat
+//
+//  Created by Anthony Abbott on 07/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MindMyHatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
